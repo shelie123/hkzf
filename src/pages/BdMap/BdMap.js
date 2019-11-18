49af50b25891f555a6195909550a04fa903bc3dd
zhongruiqiang <q730513957@163.com>
2019-11-18 17:59:34 +0800

    已优化房源代码

--- src/pages/BdMap/BdMap.js
+++ src/pages/BdMap/BdMap.js
@@ -7,16 +7,30 @@ import axios from "../../utils/request";
 
 const BMap = window.BMap;
 
-class BdMap extends Component {
-  componentDidMount() {
-    // // 在react中要使用全局变量 必须要加上window
-    // // 创建地图实例
-    // var map = new BMap.Map("allmap");
-    // // 创建点坐标
-    // var point = new BMap.Point(116.404, 39.915);
-    // // 初始化地图，设置中心点坐标和地图级别
-    // map.centerAndZoom(point, 11);
+/* 
+1 第一次加载的时候
+  1 缩放倍数 10 
+  2 label 是一个圆
+  3 再点击 放大被点击-加载被点击的房源数据
+2 第二次点击的时候 
+  1 缩放倍数 12 
+  2 label 是一个圆
+  3 移除其他没有被点击的房源数据  （点击的是花都，天河就应该被移除）
+  4 再点击 放大被点击-加载被点击的房源数据
+3 第三次点击
+  1 缩放倍数 15
+  2 label 是一个矩形
+  3 移除其他没有被点击的房源数据
+  4 再点击 
+    1 居中被点击的城市
+    2 从下往上显示被点击的 房源列表
+ */
 
+class BdMap extends Component {
+  // 地图对象 没有把它放入state中 没有必要
+  // state中的数据 都是要在视图中显示 渲染
+  Map = null;
+  componentDidMount() {
     // // 平移缩放控件
     // map.addControl(new BMap.NavigationControl());
     // // 显示比例关系
@@ -25,27 +39,62 @@ class BdMap extends Component {
     // map.addControl(new BMap.OverviewMapControl());
     // 地图 卫星  地图模式
     // map.addControl(new BMap.MapTypeControl());
-    this.getCity();
+    //     this.getCity();
+    //   }
+
+    //   getCity = async () => {
+    //     // 获取城市的名称
+    //     const cityName = this.props.cityName;
+    //     // 获取该城市的id
+    //     const id = (await axios.get("area/info?name=" + cityName)).data.body.value;
+    //     // 获取该id下的房源数据
+    //     const houses = (await axios.get("area/map?id=" + id)).data.body;
+
+    //     this.drawCircle(houses);
+
+    // 获取城市名称
+    this.Map = new BMap.Map("allmap");
+    this.Map.addControl(new BMap.NavigationControl());
+    const cityName = this.props.cityName;
+    const id = null;
+    const currentPoint = {
+      longitude: this.props.point.lng,
+      latitude: this.props.point.lat
+    };
+    this.drawHouse(cityName, id, currentPoint);
   }
 
-  getCity = async () => {
-    // 获取城市的名称
-    const cityName = this.props.cityName;
-    // 获取该城市的id
-    const id = (await axios.get("area/info?name=" + cityName)).data.body.value;
-    // 获取该id下的房源数据
-    const houses = (await axios.get("area/map?id=" + id)).data.body;
-
-    this.drawCircle(houses);
-  };
-
   // 画房源-圆圈
-  drawCircle = houses => {
-    const map = new BMap.Map("allmap");
+  //   1 封装旧的代码
+  //     1 接收两个参数
+  //       cityName
+  //       id
+  //     2 业务
+  //       1 根据该 cityName 做地图居中
+  //       2 根据 id 获取城市下的房源数据
+  //       3 有了房源数组后 循环 来画 label
+  //       4 在循环里面 给label标签 绑定点击事件
+  //         事件触发了 获取到被点击的对象
+  //           该对象中 存在
+  //           城市的名称 和 城市的 id
+
+  //   drawCircle = houses => {
+  drawHouse = async (cityName, id, currentPoint) => {
+    const map = this.Map;
+    // const map = new BMap.Map("allmap");
     // const cityPoint = new BMap.Point(this.props.point.lng, this.props.point.lat);
-    const cityName = this.props.cityName;
+    // const cityName = this.props.cityName;
     // 设置当前城市的经纬度！！！ cityName="广州市"
-    map.centerAndZoom(cityName, 11);
+    const tmpPoint = new BMap.Point(
+      currentPoint.longitude,
+      currentPoint.latitude
+    );
+    map.centerAndZoom(tmpPoint, 11);
+
+    if (!id) {
+      id = (await axios.get("area/info?name=" + cityName)).data.body.value;
+    }
+    const houses = (await axios.get("area/map?id=" + id)).data.body;
     houses.forEach(v => {
       const point = new BMap.Point(v.coord.longitude, v.coord.latitude);
       const opts = {
@@ -55,6 +104,15 @@ class BdMap extends Component {
         `<div class='${IndexCss.circle}'>${v.label} <br/> ${v.count}套 </div>`,
         opts
       );
+      label.addEventListener("click", () => {
+        //   1、获取被点击的城市名称和id
+        const name = v.label;
+        const id = v.value;
+        // this.drawHouse(name, id);
+        // 清除覆盖物
+        this.Map.clearOverlays();
+        this.drawHouse(name, id, v.coord);
+      });
       label.setStyle({
         border: "none",
         backgroundColor: "transparent"
